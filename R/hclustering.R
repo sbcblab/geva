50d981d8f59afc4d090c540e22efe1b2f9c0fd06
Itamar Nunes <nunesijg@gmail.com>
2021-01-07 21:09:15 -0300

    adapted hierarchical clustering to reclycle functions and store analysis parameters

--- R/hclustering.R
+++ R/hclustering.R
@@ -11,6 +11,9 @@
 
 #' @include asserts.R
 #' @include clusteringbase.R
+#' @include statmath.R
+
+
 
 
 # Distance metric between points to find hierarchical clusters
@@ -20,46 +23,71 @@ options.hc.metric <- c('euclidean', 'maximum', 'manhattan', 'canberra', 'binary'
 # The methods 'centroid', 'median' and 'ward' require hc.metric='euclidean'
 options.hc.method <- c('centroid', 'median', 'ward', 'single')
 
-# Performs hierarchical clustering
-geva.hcluster <- function(sv, resolution=0.25, hc.method=options.hc.method, hc.metric=options.hc.metric, ...)
+# Calculates the hierarchical tree through a cluster analysis
+calc.hclust.tree <- function(sv, resolution, hc.method=options.hc.method, hc.metric=options.hc.metric, mink.p=NULL, ...)
 {
+  hc.method = match.arg(hc.method)
+  hc.metric = match.arg(hc.metric)
+  assert.operator(resolution, `>` = 0, `<=` = 1)
+  svmatrix.norm = normalize.scale.numeric(as.matrix(sv))
+  hc = hclust.vector(X = svmatrix.norm, method = hc.method, metric = hc.metric, p = mink.p)
+  horder = order(hc$height)
+  # Finding the threshold based on resolution
+  logres = -log10(hc$height)
+  sel.outliers = 1:length(logres) %in% which.outliers(logres)
+  hrange = range(logres[!sel.outliers])
+  hsize = diff(hrange)
+  logcutpos = hsize * resolution^2 + hrange[1] # Resolution is squared to have a more comprehensible scale
+  indcutpos = which.min(logres[logres > logcutpos])
+  tcut = hc$height[indcutpos]
+  # Cutting the tree based on the found threshold
+  hctree = list(height=hc$height[horder], labels=hc$labels, order=hc$order, merge=hc$merge, threshold=tcut)
+  class(hctree) = c('hclust.geva', 'hclust')
+  hctree
+}
+
+# Performs a hierarchical clustering analysis
+geva.hcluster <- function(sv, resolution=0.3, hc.method=options.hc.method, hc.metric=options.hc.metric,
+                          cl.score.method=options.cl.score.method, ...,
+                          include.raw.results=FALSE)
+{
+  cl.score.method = assert.choices(cl.score.method)
   svmatrix = as.matrix(sv)
   hc.method = assert.choices(hc.method)
   hc.metric = assert.choices(hc.metric)
   assert.names.equal(svmatrix, colnames=c('S', 'V'))
   assert.operator(resolution, `>` = 0, `<=` = 1)
-  mink.p = ...arg(p, NULL)
+  an.pars = list(resolution=resolution,
+                 cl.score.method = cl.score.method,
+                 hc.method=hc.method,
+                 hc.metric=hc.metric)
+  if (cl.score.method == 'auto') cl.score.method = 'hclust.height'
+  mink.p = ...arg(mink.p, NULL)
+  if (!is.null(mink.p))
+    an.pars$mink.p = mink.p
   vprint("Calculating hierarchical clustering...")
-  hc = hclust.vector(X = svmatrix, method = hc.method, metric = hc.metric, p = mink.p)
-  fdt = data.frame(row.names = rownames(svmatrix))
-  horder = order(hc$height)
-  
-  # Finding the threshold based on resolution
-  logres = -log10(hc$height)
-  hrange = range(logres)
-  hsize = diff(hrange)
-  logcutpos = hsize * resolution^2 + hrange[1] # Resolution is squared to have a more comprehensible scale
-  indcutpos = which.min(logres[logres > logcutpos])
-  tcut = hc$height[indcutpos]
-  
-  # Cutting the tree based on the found threshold
-  hcx = list(height=hc$height[horder], labels=hc$labels, order=hc$order, merge=hc$merge)
-  hctree = cutree(tree=hcx, h=tcut)
-  vprint("Found %d clusters", length(unique(hctree)))
-  
+  hcx = calc.hclust.tree(svmatrix, resolution, ...)
+  tcut = hcx$threshold
+  hcl = cutree(tree=hcx, h=tcut)
+  vprint("Found %d clusters", length(unique(hcl)))
   # Getting the centroids
   centroid.method = if (inherits(sv, 'GEVASummary')) sv.method(sv)$S else options.summary[1]
-  svcents = calc.cluster.centroids(svmatrix, hctree, centroid.method)
-  svcents = as.SVTable(svcents[order(rownames(svcents)),])
-  
+  svcents = calc.cluster.centroids(svmatrix, hcl, centroid.method)
+  svcents = as.SVTable(svcents[order(rownames(svcents)),,drop=FALSE])
   # Calculating the scores based on the centroids
   distance.method = ...arg(distance.method, if (hc.metric %in% options.distance) hc.metric else options.distance[1])
-  vscores = calc.cluster.scores(sv, hctree, svcents, distance.method)
+  an.pars$distance.method = distance.method
+  hgrouping = as.factor(hcl)
+  vscores = calc.cluster.scores(sv, resolution, cl = hgrouping, hctree=hcx,
+                                sv.centroids=svcents, distance.method = distance.method)
   
   # Preparing the cluster object and finishing
-  hgrouping = as.factor(hctree)
-  moffsets = calc.cluster.offsets(sv, hctree, svcents)
-  infols = list(hc.method=hc.method, hc.metric=hc.metric)
+  moffsets = calc.cluster.offsets(sv, hcl, svcents)
+  infols = list(hc.method=hc.method,
+                hc.metric=hc.metric,
+                analysis.params=an.pars)
+  if (include.raw.results)
+    infols$raw.results = hcx
   
   new('GEVACluster',
       grouping=hgrouping,
